03c3acc9aac4071e8348ca74c6e997e63b98a4e2
Pehc <Tijolinhodopagode@gmail.com>
2025-10-07 20:58:18 -0300

    Att2 - 07/10/25

---
+++ Aula06/assets/js/iguais.js
@@ -0,0 +1,17 @@
+// Dois iguais compara valores
+numero = '10';
+
+if (numero == 10){
+    console.log("verdadeiro")
+} else {
+    console.log("Falso")
+}
+
+// Três iguais compara valores e tipos
+numero2 = '10';
+
+if (numero2 === 10){
+    console.log("verdadeiro")
+} else {
+    console.log("Falso")
+}
