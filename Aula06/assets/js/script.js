a65a3b7fc3c4b9850dc475bdd42c9d735e1a8ae8
Pehc <Tijolinhodopagode@gmail.com>
2025-10-07 22:01:10 -0300

    Att3 - 07/10/25

---
+++ Aula06/assets/js/script.js
@@ -0,0 +1,84 @@
+/*
+// Dois iguais compara valores
+numero = '10';
+
+if (numero == 10){
+    console.log("verdadeiro")
+} else {
+    console.log("Falso")
+}
+
+// Três iguais compara valores e tipos
+numero2 = '10'; // tipo String
+
+if (numero2 === 10){
+    console.log("verdadeiro")
+} else {
+    console.log("Falso")
+}
+*/
+
+//-------------------------------------------------
+
+/*
+// For
+for (i=11; i<=10; i++){
+    console.log("For: ", i);
+}
+
+// While
+i=11;
+while(i<=10){
+    console.log("While: ", i);
+    i++;
+}
+
+//Do While
+i=11;
+do {
+    console.log("Do-While: ", i);
+    i++;
+} while(i<=10);
+*/
+
+//-------------------------------------------------
+
+/*
+notas = [10, 5, 3, 9];
+
+notas.push(8);
+console.log(notas);
+
+notas.pop();
+console.log(notas);
+
+notas.unshift(6);
+console.log(notas);
+
+notas.shift(8);
+console.log(notas);
+*/
+
+//-------------------------------------------------
+
+/*
+// [] = Array 
+// {} = Objeto 
+let carro = {
+    ano: 1978,
+    modelo: "Fusca",
+    cor: "Azul"
+};
+
+let carro2 = {
+    ano: 1969,
+    modelo: "Fiat",
+    cor: "Verde"
+};
+
+console.log(carro['ano']);
+carro.ano = 2004;
+console.log(carro.ano);
+*/
+
+//-------------------------------------------------
